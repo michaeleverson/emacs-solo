a8f5d31ef718b2f506fbf0702cae0acbf7138e86
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-06-02 19:42:54 -0300

    feat: make C-g behave even without minibuffer focused

--- init.el
+++ init.el
@@ -1529,6 +1529,24 @@ and restart Flymake to apply the changes."
   (read-file-name-completion-ignore-case t)
   (read-buffer-completion-ignore-case t)
   :config
+  ;; Makes C-g behave (as seen on https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smarter/)
+  (define-advice keyboard-quit
+      (:around (quit) quit-current-context)
+    "Quit the current context.
+
+When there is an active minibuffer and we are not inside it close
+it.  When we are inside the minibuffer use the regular
+`minibuffer-keyboard-quit' which quits any active region before
+exiting.  When there is no minibuffer `keyboard-quit' unless we
+are defining or executing a macro."
+    (if (active-minibuffer-window)
+        (if (minibufferp)
+            (minibuffer-keyboard-quit)
+          (abort-recursive-edit))
+      (unless (or defining-kbd-macro
+                  executing-kbd-macro)
+        (funcall-interactively quit))))
+
   ;; Keep the cursor out of the read-only portions of the.minibuffer
   (setq minibuffer-prompt-properties
         '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
