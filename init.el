356c43b3716c883bb8711f3de49a11ec60689d0e
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-06-14 22:50:35 -0300

    feat(exec-path-from-shell): new shells support
    Extended support for fish and zsh, while bash still works.

--- init.el
+++ init.el
@@ -2570,15 +2570,30 @@ Replacing `Git-' with a branch symbol."
   :defer t
   :init
   (defun emacs-solo/set-exec-path-from-shell-PATH ()
-    "Set up Emacs' `exec-path' and PATH environment the same as user Shell."
+    "Set up Emacs' `exec-path' and PATH environment the same as the user's shell.
+This works with bash, zsh, or fish)."
     (interactive)
-    (let ((path-from-shell
-           (replace-regexp-in-string
-            "[ \t\n]*$" "" (shell-command-to-string
-                            "$SHELL --login -c 'echo $PATH'"))))
-      (setenv "PATH" path-from-shell)
-      (setq exec-path (split-string path-from-shell path-separator))
-      (message ">>> emacs-solo: PATH loaded")))
+    (let* ((shell (getenv "SHELL"))
+           (shell-name (file-name-nondirectory shell))
+           (command
+            (cond
+             ((string= shell-name "fish")
+              "fish -c 'string join : $PATH'")
+             ((string= shell-name "zsh")
+              "zsh -i -c 'printenv PATH'")
+             ((string= shell-name "bash")
+              "bash --login -c 'echo $PATH'")
+             (t nil))))
+      (if (not command)
+          (message "emacs-solo: Unsupported shell: %s" shell-name)
+        (let ((path-from-shell
+               (replace-regexp-in-string
+                "[ \t\n]*$" ""
+                (shell-command-to-string command))))
+          (when (and path-from-shell (not (string= path-from-shell "")))
+            (setenv "PATH" path-from-shell)
+            (setq exec-path (split-string path-from-shell path-separator))
+            (message ">>> emacs-solo: PATH loaded from %s" shell-name))))))
 
   (defun emacs-solo/fix-asdf-path ()
     "Ensure asdf shims and active Node.js version's bin directory are first in PATH."
